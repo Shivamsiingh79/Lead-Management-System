f618c4dc7efd6f070ebf1e18e02ecd0955b8ef29
Akshay kumar tyagi <79367523+akshaytyagi482@users.noreply.github.com>
2025-12-17 17:11:22 +0530

    m

--- Frontend/my-react-app/src/context/AuthContext.jsx
+++ Frontend/my-react-app/src/context/AuthContext.jsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
 
       try {
         const { data } = await axios.get(
-          "http://localhost:5000/api/auth/profile",
+          `${import.meta.env.VITE_API_URL}/auth/profile`,
           {
             headers: {
               Authorization: `Bearer ${token}`
@@ -49,7 +49,7 @@ export const AuthProvider = ({ children }) => {
    */
   const login = async (email, password) => {
     const { data } = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      `${import.meta.env.VITE_API_URL}auth/login`,
       { email, password }
     );
 
